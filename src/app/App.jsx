5753d721f005df772c868c29ec13548d5a866268
MAbdurahman <mdbdrrhm@gmail.com>
2023-10-05 07:34:32 -0500

    Complete project

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useGeoLocation } from "../hooks/useGeoLocation";
 
 export default function App() {
+    const {
+        isLoading,
+        position: { lat, lng },
+        error,
+        getPosition
+    } = useGeoLocation();
+    const [countClicks, setCountClicks] = useState(0);
+
+    function handleClick() {
+        setCountClicks((count) => count + 1);
+        getPosition();
+    }
 
     return (
-        <div>
-            <h1>reactjs-geo-location-example</h1>
-        </div>
+        <div className={'wrapper'}>
+            <button className={'button'} onClick={handleClick} disabled={isLoading}>
+                Get Location
+            </button>
 
+            {isLoading && <p>Loading position...</p>}
+            {error && <p className={'paragraph'}>{error}</p>}
+            {!isLoading && !error && lat && lng && (
+                <p className={'paragraph'}>
+                    Your GPS Position:{" "}
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+                    >
+                        {lat}, {lng}
+                    </a>
+                </p>
+            )}
+
+            <p className={'paragraph'}>You requested position {countClicks} times</p>
+        </div>
     );
 };
